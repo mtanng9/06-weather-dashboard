77236d590019c9fcc9cbda61b3aace7f00941e14
mtanng9 <mlt152498@gmail.com>
2023-01-11 20:10:29 -0500

    added outline of what to do in JS

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// Grab city input on search button click
+// get weather info
+// save to local storage
+// display in history list
+
+// grab city on history item click
+// get weather info
+
+// show current weather of "current" city
+// city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+
+// show 5 day weather of "current" city
+// date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
